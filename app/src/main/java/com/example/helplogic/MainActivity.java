e45a838fffac4eb19c70f34129bf3ae567366c5c
pepes7 <42782156+pepes7@users.noreply.github.com>
2019-02-25 02:18:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/helplogic/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.helplogic;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button botaoEntrar,botaoCadastrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        botaoEntrar = findViewById(R.id.botaoEntrar);
+        botaoCadastrar = findViewById(R.id.botaoCadastrar);
+
+        botaoEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),EscolhaActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        botaoCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
