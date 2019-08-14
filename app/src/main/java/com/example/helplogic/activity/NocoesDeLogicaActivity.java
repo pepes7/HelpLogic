a3f5e3dd667cd09d5eca9443a206d7f3be50b9c0
Gladson <gladsonsouza@live.com>
2019-08-14 15:14:55 -0400

    Lógica de inicalização dos conteúdos em página única implementado

--- app/src/main/java/com/example/helplogic/activity/NocoesDeLogicaActivity.java
+++ app/src/main/java/com/example/helplogic/activity/NocoesDeLogicaActivity.java
@@ -1,9 +1,7 @@
 package com.example.helplogic.activity;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.helplogic.R;
 
